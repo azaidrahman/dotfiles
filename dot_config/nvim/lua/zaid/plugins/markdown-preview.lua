e5035daf70d0a1655cc7056cb2a644d63d564a91
Zaid <azaidrahman@gmail.com>
2026-08-27 23:34:48 +0800

    feat(nvim): add markdown preview reader themes

--- dot_config/nvim/lua/zaid/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/zaid/plugins/markdown-preview.lua
@@ -4,6 +4,17 @@ return {
     ft = { "markdown" },
     build = "cd app && yarn install",
     init = function()
+        local reader_style = "sepia" -- sepia, paper, or obsidian
+        local css_dir = vim.fn.stdpath("config") .. "/assets/markdown-preview"
+
         vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_theme = "light"
+        vim.g.mkdp_markdown_css = string.format("%s/%s.css", css_dir, reader_style)
+        vim.g.mkdp_page_title = "${name} - Markdown"
+        vim.g.mkdp_preview_options = {
+            disable_filename = 1,
+            hide_yaml_meta = 1,
+            sync_scroll_type = "middle",
+        }
     end,
 }
